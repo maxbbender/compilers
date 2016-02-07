63db9ca96fc0663eadf762a791a7639582733311
maxbbender <maxbbender@gmail.com>
2016-02-07 16:36:10 -0500

    Change because we are now using Java not typescript

---
+++ Compilers/src/lexer/LexerMain.java
@@ -0,0 +1,48 @@
+package lexer;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class LexerMain {
+
+	public static void main(String[] args) {
+		try {
+			String[] splitString = fileProcess("lib/code.txt"); 
+			
+			for (String string : splitString) {
+				if ()
+			}
+			
+		} catch (IOException e) { 
+			System.out.println("Error on the fileProcess" + e);
+		}
+		
+		
+	}
+	
+	public static String[] fileProcess(String fileName) throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader(fileName));
+		
+		try {
+			String[] splitString;
+			StringBuilder sb = new StringBuilder();
+			String line = br.readLine();
+			
+			while (line != null ) {
+				sb.append(line);
+				sb.append(" ");
+				line = br.readLine();
+			}
+			return sb.toString().split(" +");
+		} finally {
+			br.close();
+		}
+	}
+	
+	public static boolean checkForKeywords() {
+		
+	}
+}
