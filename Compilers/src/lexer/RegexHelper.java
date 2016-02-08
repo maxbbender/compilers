c2417fc24a6a6f7caca770638f2869efb64030d7
maxbbender <maxbbender@gmail.com>
2016-02-08 16:44:24 -0500

    Regex update bringing together keywords and literals

--- Compilers/src/lexer/RegexHelper.java
+++ Compilers/src/lexer/RegexHelper.java
@@ -1,16 +1,26 @@
 package lexer;
 
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegexHelper {
-	private final static String alpha = "([a-z])";
+	private final static String id = "([a-z])";
+	private final static String alpha = "(\\w)";
 	private final static String digits = "(\\d+)";
-	private final static String strings = "\".*\"";
+	private final static String strings = "(\"[^\"].*\")";
 	private final static String equality = "(==)";
 	private final static String notEquality = "(!=)";
-	private final static String space = ("(\\S)");
-	private final static String assignment = ("(=)");
+	private final static String space = "(\\S)";
+	private final static String assignment = "(=)";
+	private final static String ifKeyword = "(if)";
+	private final static String whileKeyword = "(while)";
+	private final static String printKeyword = "(print)";
+	private final static String intKeyword = "(int)";
+	private final static String stringKeyword = "(string)";
+	private final static String booleanKeyword = "(boolean)";
+	private final static String booleanTrue = "(true)";
+	private final static String booleanFalse = "(false)";
 	
 	
 	public RegexHelper(String checkString) {
@@ -34,6 +44,21 @@ public class RegexHelper {
 	
 	public static String[] parseInput(String input) {
 		ArrayList tokens = new ArrayList();
+		String keywords = 
+				ifKeyword + "|" + 
+				whileKeyword  + "|" + 
+				printKeyword  + "|" + 
+				stringKeyword + "|" + 
+				booleanKeyword + "|" + 
+				booleanTrue + "|" +
+				booleanFalse;
+		String literals = 
+				strings + "|" + 
+				id + "|" +
+				equality + "|" +	
+				notEquality  + "|" +
+				assignment  + "|" +
+				
 		String regexParser = alpha + digits + strings + equality + notEquality + space + assignment;
 		Pattern p = Pattern.compile(regexParser);
 		Matcher m = p.matcher(input);
