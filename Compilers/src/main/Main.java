e328f9e29b8b0cea2b1ac7fa98ca84f7509945e0
maxbbender <maxbbender@gmail.com>
2016-02-25 12:20:04 -0500

    updated to prompt for file url and verbose mode

--- Compilers/src/main/Main.java
+++ Compilers/src/main/Main.java
@@ -1,4 +1,5 @@
 package main;
+import java.io.File;
 import java.util.Iterator;
 
 import lexer.LexerMain;
@@ -7,6 +8,7 @@ import parser.ParserMain;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.Scanner;
 public class Main {
 	private final static Logger log = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 	private static LogFormatter formatter;
@@ -15,11 +17,43 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		int numberOfPrograms = 0;
-		LexerMain lexer = new LexerMain(args[0]);
+		boolean verbose = false;
+		String temp = null;
+		String temp2 = null;
+		boolean valid = false;
+		LexerMain lexer = null;
 		ParserMain parser;
-		log.setLevel(Level.WARNING);
-		//formatter = new LogFormatter();
-		//log.set(formatter);
+		Scanner input = new Scanner(System.in);
+		
+		/* What is the file path for code? */
+		while (!valid) {
+			System.out.println("What is the filePath for the code?");
+			temp = input.next();
+			File f = new File(temp);
+			if(f.exists() && !f.isDirectory()) { 
+			    valid = true;
+			} else {
+				System.out.println("File is not found please try again");
+			}
+		}
+		valid = false; 
+		/* Are we going to run in Verbose? */
+		while (!valid) {
+			System.out.println("Should we run in verbose? (y/n)");
+			temp2 = input.next();
+			if (temp2.equals("y")) {
+				log.setLevel(Level.INFO);
+				valid = true;
+			} else if (temp2.equals("n")) {
+				log.setLevel(Level.WARNING);
+				valid = true;
+			} else {
+				System.out.println("Invalid input, try again");
+			}
+		}
+		/* Initiate the Lexer with the input */
+		lexer = new LexerMain(temp);
+		
 		if (!lexer.getMyRegex().hasErrors()) {
 			System.out.println("-------------------");
 			System.out.println("-------LEXER-------");
